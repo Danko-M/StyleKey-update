40c72dc564cc4f88d25ec30eb122e8d141162200
Danko-M <dankomilutinovic@gmail.com>
2015-04-18 21:11:05 +0200

    added some html,css and js comments

--- js/info-modals.js
+++ js/info-modals.js
@@ -1,12 +1,21 @@
+/* 
+Author: Danko Milutinovic
+Desctiption: 
+  This jQuery code is for modal windows which 
+  appears on pages: measurements-men.html 
+  and measurements-women.html
+Contact:  dankomilutinovic@gmail.com, 
+          https://rs.linkedin.com/pub/danko-milutinovic/a3/675/778 
+*/
+
 $(function () {
   var modalSettings = {
     effect : 'fadein',
-    // position :'center, center',
     overlayColor :'#fff',
     overlayOpacity :'0.5'
   };
 
-  // men
+  // man
 
   var $modItemsMan = $('.height-m, .chest-m, .waist-m, .upperbody-m, .waisband-m, .hips-m, .shoulder-m, .sleevelength-m, .outseam-m, .inseam-m, .neck-m, .jacketlength-m, .bicep-m, .wrist-m, .rise-m, .thigh-m, .knee-m');
 
@@ -15,7 +24,7 @@ $(function () {
     e.preventDefault();
   });
 
-  // women
+  // woman
 
   var $modItemsWoman = $('.height-w, .bust-w, .waist-w, .hips-w, .shoulder-w, .sleevelength-w, .waistpoint-w, .outseam-w, .inseam-w, .neck-w, .underbust-w, .bicep-w, .wrist-w, .bustpoint-w, .rise-w, .thigh-w, .knee-w');
 
